a9569958c9b56e0abeff99c0bdeb7f62d01c5de2
lithium <code@hlidskialf.com>
2012-12-27 23:02:59 -0800

    basic demo

---
+++ Makefile
@@ -0,0 +1,13 @@
+APP = demo1
+
+include $(SDK_DIR)/Makefile.defs
+
+OBJS = \
+	$(ASSETS).gen.o \
+	main.o
+
+ASSETDEPS += \
+	assets/*.png \
+	$(ASSETS).lua
+
+include $(SDK_DIR)/Makefile.rules
---
+++ assets.lua
@@ -0,0 +1,10 @@
+IconAssets = group{quality=9.95}
+Icon = image{"assets/icon.png"}
+
+
+BootstrapAssets = group{ quality=9.9 }
+Star = image{ "assets/star8.png", pinned=true }
+SfxConnect = sound{ "assets/connect.raw" }
+Music = tracker{"assets/bitshift.xm"}
+
+-- Backgrounds = image{ "assets/bg.png", }
